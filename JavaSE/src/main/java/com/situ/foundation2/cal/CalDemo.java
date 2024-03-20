581c15fe3005e35a0272c3c604de13bdd9a2a707
zyc <2293911753@qq.com>
2024-03-20 15:40:06 +0800

    继承和多态1

--- JavaSE/src/main/java/com/situ/foundation2/cal/CalDemo.java
+++ JavaSE/src/main/java/com/situ/foundation2/cal/CalDemo.java
@@ -1,21 +1,41 @@
 package com.situ.foundation2.cal;
 
+import org.junit.Test;
+
 public class CalDemo {
+
     public static void main(String[] args) {
-        Add add = new Add(3, 5);
-        int result = add.getResult();
-        System.out.println(result);
+//        Add add = new Add(3, 5);
+//        int result = add.getResult();
+//        System.out.println(result);
 
-        Sub sub = new Sub(3, 5);
-        int result1 = sub.getResult();
-        System.out.println(result1);
+//        Sub sub = new Sub(3, 5);
+//        int result1 = sub.getResult();
+//        System.out.println(result1);
+//
+//        Multi multi = new Multi(3, 5);
+//        int result2 = multi.getResult();
+//        System.out.println(result2);
+//
+//        Div div = new Div(3, 5);
+//        int result3 = div.getResult();
+//        System.out.println(result3);
 
-        Multi multi = new Multi(3, 5);
-        int result2 = multi.getResult();
-        System.out.println(result2);
+        //多态
+        Cal cal = new Add(3, 5);
+        System.out.println(cal.getResult());
 
-        Div div = new Div(3, 5);
-        int result3 = div.getResult();
-        System.out.println(result3);
+        cal = new Sub(3, 5);
+        System.out.println(cal.getResult());
+
+        cal = new Multi(3, 5);
+        System.out.println(cal.getResult());
+
+        cal = new Div(3, 5);
+        System.out.println(cal.getResult());
+
+
     }
+
+
 }
