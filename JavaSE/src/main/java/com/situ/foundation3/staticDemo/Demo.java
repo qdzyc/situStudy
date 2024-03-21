03708e32a2fd1c5b6ab52b2b321a77f4e4a1d51e
zyc <2293911753@qq.com>
2024-03-21 11:48:58 +0800

    静态与非静态

---
+++ JavaSE/src/main/java/com/situ/foundation3/staticDemo/Demo.java
@@ -0,0 +1,12 @@
+package com.situ.foundation3.staticDemo;
+
+import org.junit.Test;
+
+public class Demo {
+    @Test
+    public void test(){
+        Student student = new Student();
+        student.show();
+        student.id = 1;
+    }
+}
---
+++ JavaSE/src/main/java/com/situ/foundation3/staticDemo/Student.java
@@ -0,0 +1,32 @@
+package com.situ.foundation3.staticDemo;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    //属性、成员变量
+    //非静态的、需要new对象使用
+    protected int id;
+    protected String name;
+    //静态变量
+    public static String country = "中国";
+
+    public void show() {
+        System.out.println("Student.show");
+    }
+
+    //类加载的时候静态的就已经存在了，先后关系来说，静态的早于动态的而设置，所以无法调用动态的
+    //静态的只能调用静态的，无论是方法还是属性
+    public static void print() {
+        System.out.println("Student.print");
+        country = "中";
+    }
+
+    public Student() {
+    }
+
+    public Student(int id, String name, int age, String gender) {
+        this.id = id;
+        this.name = name;
+    }
+}
